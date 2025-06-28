c9192c3fe69f3992ca2403725f0bf62a02081349
mulkah-dev <mulkah.akala@gmail.com>
2025-06-28 23:32:51 +0100

    Add connection file

---
+++ MySQLServer.py
@@ -0,0 +1,30 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Ayman&Asmaa1",
+    #database = "alx_book_store"
+
+)
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(f"Error while connecting to MySQL: {e}")
+
+finally:
+    # Clean up: close cursor and connection
+    if 'mycursor' in locals() and mycursor is not None:
+        mycursor.close()
+    if mydb.is_connected():
+        mydb.close()
+
+
+
+mycursor.close()
+mydb.close()
