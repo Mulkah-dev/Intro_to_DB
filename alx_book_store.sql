ee1a777db86a632de281b1712de3fa53166df474
mulkah-dev <mulkah.akala@gmail.com>
2025-06-28 22:53:10 +0100

    Add new edits

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,13 +3,13 @@ USE alx_book_store;
 
 CREATE TABLE Authors(
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215),
+    author_name VARCHAR(215)
 )
 CREATE TABLE Books(
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130),
-    author_id, 
-    FOREIGN KEY(author_id) REFERENCES Authors(author_id),
+    author_id INT, 
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
     price DOUBLE,
     publication_date DATE
 )
@@ -26,7 +26,7 @@ CREATE TABLE Customers(
 CREATE TABLE Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 )
 
@@ -34,7 +34,7 @@ CREATE TABLE Order_Details(
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE 
 )
