f4ba16523a5f18c730fe002678a23fce6382db20
mulkah-dev <mulkah.akala@gmail.com>
2025-06-28 22:48:28 +0100

    Add new changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,12 +25,15 @@ CREATE TABLE Customers(
 
 CREATE TABLE Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
     FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 )
 
 CREATE TABLE Order_Details(
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
     FOREIGN KEY(order_id) REFERENCES Orders(order_id),
     FOREIGN KEY(book_id) REFERENCES Books(book_id),
     quantity DOUBLE 
